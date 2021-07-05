dfc285d7c4b5890a1de279359f8e3b278fc818ea
Anand Kumar <anand22121996@gmail.com>
2021-07-05 08:51:35 +0530

    postgresql db added

--- password/settings.py
+++ password/settings.py
@@ -80,11 +80,22 @@ WSGI_APPLICATION = 'password.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+DATABASES={
+   'default':{
+      'ENGINE':'django.db.backends.postgresql_psycopg2',
+      'NAME':'password_db',
+      'USER':'postgres',
+      'PASSWORD':'root',
+      'HOST':'localhost',
+      'PORT':'5432',
+   }
 }
 
 
